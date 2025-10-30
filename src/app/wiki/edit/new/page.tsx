7c37cc48f06069ae44a294041d1899003d12dd75
zshcole <colburnsanders@icloud.com>
2025-10-29 22:32:27 -0500

    add server actions

--- src/app/wiki/edit/new/page.tsx
+++ src/app/wiki/edit/new/page.tsx
@@ -1,5 +1,7 @@
 import WikiEditor from "@/components/wiki-editor";
+import { stackServerApp } from "@/stack/server";
 
-export default function NewArticlePage() {
+export default async function NewArticlePage() {
+  await stackServerApp.getUser({ or: "redirect" });
   return <WikiEditor isEditing={false} />;
 }

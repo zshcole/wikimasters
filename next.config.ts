9581e7726f98b6e130ac98f11bab84c512a152e6
zshcole <colburnsanders@icloud.com>
2025-11-04 19:06:32 -0600

    lint fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL(`${process.env.BLOB_BASE_URL}/**`)]
+    remotePatterns: [new URL(`${process.env.BLOB_BASE_URL}/**`)],
   },
   turbopack: {
     root: dirname(__filename),
